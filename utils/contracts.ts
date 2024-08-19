8ce1fa767ef3defe76488c077245d08ec264fed5
mhusnirizal <mochhusnir@gmail.com>
2024-08-20 02:05:50 +0800

    Replace with new contract

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -6,7 +6,7 @@ import { STAKING_CONTRACT_ABI } from './stakingContractABI';
 // Replace <contract_address> with the contract address of your contract
 const stakeTokenContractAddress = '0x719058944974A4825a5689Bb9f9A73B445730959';
 const rewardTokenContractAddress = '0x5141A4cEa1e24736426F7aeCC61AcC509119072b';
-const stakingContractAddress = '0xcc88DBD545400a958dac78eb82312cb742CB4E36';
+const stakingContractAddress = '0x25f16D8A3f0CcD65098F853Ff2A834a63F4D62EA';
 
 export const STAKE_TOKEN_CONTRACT = getContract({
   client: client,
